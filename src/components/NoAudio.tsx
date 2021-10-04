b3ab7e54db65497417cdb96f963d754e31183dac
George <71434803+george-swift@users.noreply.github.com>
2021-10-04 02:55:27 +0100

    Add fallback component

---
+++ src/components/NoAudio.tsx
@@ -0,0 +1,11 @@
+const NoAudio = ():JSX.Element => (
+  <div>
+    <p>
+      Unfortunately your browser does not support
+      <code>Audio API</code>
+    </p>
+    <p>Virtual Keyboard depends on it to work :(</p>
+  </div>
+);
+
+export default NoAudio;
